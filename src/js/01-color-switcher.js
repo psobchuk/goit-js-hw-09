2a03e614b899566bcd47440cfcaa2e5c47b8e745
Petro Sobchuk <sobchuk.p@gmail.com>
2023-01-21 19:10:35 +0200

    01-color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
 
+let timerId = null;
+
+startButton.addEventListener('click', () => {
+  startButton.disabled = true;
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+stopButton.addEventListener('click', () => {
+  startButton.disabled = false;
+  clearInterval(timerId);
+});
+
+// Герерування випадкового кольору
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
